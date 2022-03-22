0bb49e360dd761873ed884aabb307a0149b3922a
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-03-22 20:30:41 -0300

    Requisito 5 iniciado

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,41 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('teste do componente Pokedex', () => {
+  // Ok
+  test('Se contém um heading h2 com o texto Encountered pokémons', () => {
+    renderWithRouter(<App />);
+    const texto = screen.getByRole('heading',
+      { name: /Encountered pokémons/i,
+        level: 2 });
+    expect(texto).toBeInTheDocument();
+  });
+});
+
+describe('Teste se é exibido opróximo Pokémon da lista', () => {
+  // Ok
+  test('O botão deve conter o texto Próximo pokémon', () => {
+    renderWithRouter(<App />);
+    const btn = screen.getByText('Próximo pokémon');
+    expect(btn).toBeInTheDocument();
+  });
+
+  // test('Mostrar próximos Pokémons ao clicar no botão', () => {
+  //   renderWithRouter(<App />);
+  // });
+
+  // test('Mostrar primeiro pokemon ao clicar no botao es estiver no último', () => {
+  //   renderWithRouter(<App />);
+  // });
+});
+
+describe('Teste se é mostrado apenas um Pokémon por vez', () => {
+  test('Teste se é mostrado apenas um Pokémon por vez', () => {
+    renderWithRouter(<App />);
+    const pokemon1 = screen.getAllByTestId('pokemon-name');
+    expect(pokemon1).toHaveLength(1);
+  });
+});
