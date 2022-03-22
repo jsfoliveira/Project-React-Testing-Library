074d6d49c9515a5c22ddaa2ccdad2377761a5f6f
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-03-22 18:44:47 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+describe('teste do componente FavoritePokemons', () => {
+  test('É exibido No favorite pokemon found, se não tiver pokémons favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const notFound = screen.getByText(/No favorite pokemon found/i);
+    expect(notFound).toBeInTheDocument();
+  });
+
+  test('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    // RESOLUÇÃO: quando eu clicar no More details do card, deve me direcionar para '/pokemons/4', o pathname deve ser esse. Quando preencher o checkbox, deve ser redirecionado para /favorites. No final, deve aparecer o charmander, pois ele que está no '/pokemons/4'.
+    const { history } = renderWithRouter(<App />);
+    const btndetails = screen.getByText(/More details/i);
+    userEvent.click(btndetails);
+    history.push('/pokemons/4');
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/4');
+
+    const checked = screen.getByRole('checkbox', { name: /Pokémon favoritado?/i });
+    userEvent.click(checked);
+    history.push('/favorites');
+    const pokemon = screen.getByText(/Charmander/i);
+    expect(pokemon).toBeDefined();
+  });
+});
